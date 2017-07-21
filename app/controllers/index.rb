6b5968ee2b3c0da6bdc2833171c6a3bac43146f0
Sofia Forsell <email": null>
2017-07-21 17:00:25 -0400

    add login routes, logic for showing the right links in header, add error handling on login page and some basic css

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,10 +10,37 @@ end
 post '/users' do
   @user = User.new(params)
   if @user.save
+    session[:user_id] = user.id
     redirect '/'
   else
     status 422
     @errors = @user.errors.full_messages
     erb :'/users/new'
   end
+end
+
+get "/sessions/new" do
+  erb :'/users/login'
+end
+
+post "/sessions" do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    status 400
+    @errors = ["Oops! Was that the right login info? try again"]
+    erb :"/users/login"
+  end
+end
+
+get "/users/show" do
+  "Welcome #{current_user.username}! Here are your stats!"
+
+end
+
+get "/logout" do
+  session.delete(:user_id)
+  redirect "/"
 end
