c69e2e040adb2bd7cdaf7b1392d9cdda91db3386
Eddie Harmon <eddieharmon@hotmail.com>
2017-07-21 15:55:35 -0400

    add new user get and post route. add current user helper method. add error handling partial.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,18 @@
 get '/' do
 end
+
+
+get '/users/new' do
+  erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params)
+  if @user.save
+    redirect '/'
+  else
+    status 422
+    @errors = @user.errors.full_messages
+    erb :'/users/new'
+  end
+end
